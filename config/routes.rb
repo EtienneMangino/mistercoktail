7b7385f35f5bbee78947725aa0513a5e08b93b04
EtienneMangino <etiennemangino@gmail.com>
2014-11-13 11:47:30 +0100

    all models generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :ingredients
+
   resources :coktails
+
+  root to: "coktails#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
